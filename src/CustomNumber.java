a10afbd7fce8b79ed12aed97af0442149bc97788
Giulia <giuliac35@gmail.com>
2023-02-10 11:22:25 +0100

    Create CustomNumber object and invoke getMultiplicationTable method

--- src/CustomNumber.java
+++ src/CustomNumber.java
@@ -1,16 +1,22 @@
 public class CustomNumber {
     private int number;
 
-    public CustomNumber(int number){
-        this.number = number;
+    public CustomNumber(){
         int min = 1;
         int max = 12;
         number = (int) Math.floor(Math.random() *(max - min + 1) + min);
     }
 
-    public void getMultiplicationTable(int number){
-        for(int i = 0; i<=12; i++){
+    public void getMultiplicationTable(){
+        for(int i = 12; i>=1; i--){
             System.out.println(number * i);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "CustomNumber{" +
+                "number=" + number +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
+        CustomNumber customNumber = new CustomNumber();
 
+        customNumber.getMultiplicationTable();
     }
 }
